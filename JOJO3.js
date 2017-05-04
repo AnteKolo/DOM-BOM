8c790697a520c73a2a76f65c4e4dbd553861e17d
AnteKolo <ante.kolobaric@yahoo.com>
2017-05-04 10:01:01 +0300

    Add files via upload

---
+++ JOJO3.js
@@ -0,0 +1,7 @@
+function gum() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    document.getElementById("s").innerHTML = "Sirina prozora je " + w + ".";
+    document.getElementById("v").innerHTML = "Visina prozora je " + h + ".";
+    window.open("http://www.khlzagreb.hr/UserFiles/Image/gallery/mladi-juniori/m/Joja.jpg");
+}
